948e47cc9016ea420be316738bc6a83f0f029386
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-10-27 14:19:19 +0100

    Test.

--- LambdaCode/app.py
+++ LambdaCode/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello worlddd",
+            "message": "hello worldddd",
             # "location": ip.text.replace("\n", "")
         }),
     }
