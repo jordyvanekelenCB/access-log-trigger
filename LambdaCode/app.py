fde4b4b732c8efcb5be9afe570650a77b9faa8b3
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-10-27 15:41:04 +0100

    Added base code.

--- LambdaCode/app.py
+++ LambdaCode/app.py
@@ -1,42 +1,65 @@
+import logging
 import json
+import boto3
+from io import BytesIO
+import gzip
 
-# import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
+s3 = boto3.client('s3')
+
+def detect_http_flood(logfile):
+    return ""
+    # Loop through logfile
+    # Create class object
+    # Create list of class object
+    # IP address = key, requests = value
+    # if exists in list, add, if not, create new value
+
+
+def parse_access_log_line(line):
+    # Do something intelligent ... ?
+    logger.info(line);
+
+def decode_gzip(gzip_lines):
+    gzip_file = BytesIO(gzip_lines)
+    gzip_file = gzip.GzipFile(fileobj=gzip_file)
+    decoded_zip = gzip_file.read()
+
+    return decoded_zip
+
+def retrieve_gzip_lines_from_bucket(event):
+
+    # retrieve bucket name and file_key from the S3 event
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_key = event['Records'][0]['s3']['object']['key']
+
+    logger.info('Reading {} from {}'.format(file_key, bucket_name))
+
+    # get the object
+    log_obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+
+    # get encoded gzip file
+    gzip_lines = log_obj['Body'].read()
+
+    return gzip_lines
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    logger.info("FLAG")
+    logger.info(event)
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    # Retrieve GZIP lines
+    gzip_lines = retrieve_gzip_lines_from_bucket(event);
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    # Decode gzipped access log file.
+    decoded_access_log_file = decode_gzip(gzip_lines);
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    logger.info(decoded_access_log_file)
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
+    splitted_lines = decoded_access_log_file.split(b'\n')
 
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello worlddddddd",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    # Parse each line
+    for line in splitted_lines:
+        parse_access_log_line(line)
