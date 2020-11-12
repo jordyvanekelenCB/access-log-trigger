e7ea455ea79295a475826f34db1d4fb65946b368
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-11-12 11:58:28 +0100

    Fixed AWSS3 for pylint.

--- LambdaCode/connection/aws_s3_connection.py
+++ LambdaCode/connection/aws_s3_connection.py
@@ -1,19 +1,24 @@
+""" Handles connection to S3 """
+
 import logging
 from io import BytesIO
 import gzip
 from connection.aws_connection import AWSConnection
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# Setup logger
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 
 class AWSS3:
+    """ This class is responsible for handling S3 connections and returning ALB log files from buckets """
 
     def __init__(self, aws_event):
         self.aws_event = aws_event
         self.boto_s3_client = AWSConnection().get_connection('s3')
 
     def get_aws_log_file(self):
+        """ Main function """
 
         # Retrieve gzip file
         encoded_gzip_file = self.retrieve_gzip_file_from_bucket()
@@ -24,11 +29,14 @@ class AWSS3:
         return decoded_gzip_file
 
     def retrieve_gzip_file_from_bucket(self):
+        """ Retrieves encoded gzip file from bucket """
 
         # Retrieve bucket name and file-key from the Lambda event
         bucket_name = self.aws_event['Records'][0]['s3']['bucket']['name']
         file_key = self.aws_event['Records'][0]['s3']['object']['key']
-        logger.info('Reading {} from {}'.format(file_key, bucket_name))
+
+        # pylint: disable=W1202
+        LOGGER.info('Reading {} from {}'.format(file_key, bucket_name))
 
         # Retrieve the ALB log file from the bucket by file-key
         log_obj = self.boto_s3_client.get_object(Bucket=bucket_name, Key=file_key)
@@ -38,7 +46,9 @@ class AWSS3:
 
         return gzip_file
 
-    def decode_gzip_file(self, gzip_lines):
+    @staticmethod
+    def decode_gzip_file(gzip_lines):
+        """ Decodes the gzip file """
 
         # Convert gzip file to buffer
         gzip_file_buffer = BytesIO(gzip_lines)
