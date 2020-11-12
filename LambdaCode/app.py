8962e50616d1b91c93f867a851898d3ddb55317e
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-11-12 11:16:34 +0100

    Fixed app for pylint.

--- LambdaCode/app.py
+++ LambdaCode/app.py
@@ -1,19 +1,24 @@
+"""
+Entry point file
+"""
+import os
 import logging
+import configparser
 from utilities import ALBLogParser
 from connection.aws_s3_connection import AWSS3
 from http_flood import HTTPFlood
-import configparser
-import os
 
 # Setup logger
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 # Setup config parser
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), 'config', 'config.ini'))
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.join(os.path.dirname(__file__), 'config', 'config.ini'))
 
+# pylint: disable=W0613
 def lambda_handler(event, context):
+    """ Entry point of the application"""
 
     # Instantiate aws s3 helper
     aws_s3_helper = AWSS3(event)
@@ -26,7 +31,7 @@ def lambda_handler(event, context):
     alb_log_array = alb_log_parser_helper.parse_alb_log_file(alb_log_file)
 
     # Activate HTTP flood detection
-    http_flood = HTTPFlood(config, alb_log_array)
+    http_flood = HTTPFlood(CONFIG, alb_log_array)
     http_flood_results = http_flood.detect_http_flood()
 
     # Print results
@@ -34,10 +39,13 @@ def lambda_handler(event, context):
 
 
 def print_results(http_flood_results):
+    """ Prints the results of the http flood detection module """
 
-    logger.info('================================ Http flood detection results ================================')
+    LOGGER.info('================================ Http flood detection results ================================')
 
     for parsed_alb_client in http_flood_results:
-        logger.info("Finding: " + "Client ip: " + parsed_alb_client.client_ip + ' | Flood level: ' + str(parsed_alb_client.http_flood_level.name) + ' | Number of requests: ' + str(parsed_alb_client.number_of_requests))
 
-
+        # pylint: disable=W1202
+        LOGGER.info("Finding: Client ip: {0} | Flood level: {1} | Number of requests: {2}"
+                    .format(parsed_alb_client.client_ip, parsed_alb_client.http_flood_level.name,
+                            parsed_alb_client.number_of_requests))
